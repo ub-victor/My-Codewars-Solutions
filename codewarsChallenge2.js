f539069c85f75f232e4c0315c15917520043d764
ub-victor <victoireushindi371@gmail.com>
2025-01-18 15:01:28 +0200

    correct js2 and 3

--- codewarsChallenge2.js
+++ codewarsChallenge2.js
@@ -1,11 +1,13 @@
-// // This code iterates through the 'numbers' array and logs each number doubled to the console.
+//This code iterates through the 'numbers' array and logs
+//  each number doubled to the console.
 /*let numbers = [1,2,3,4,5,6];
 
 numbers.forEach(number =>{
 console.log(number*2)
 });*/
 
-// This code iterates through the 'numbers' array, logs each number doubled along with its index and the complete array.
+// This code iterates through the 'numbers' array, logs each number 
+// doubled along with its index and the complete array.
 
 
 let numbers = [1,2,3,4,5,6];
