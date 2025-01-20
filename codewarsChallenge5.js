d2bc7064520d951764e2baf98db15c67c76dd9cf
ub-victor <victoireushindi371@gmail.com>
2025-01-20 09:34:44 +0200

    add some comment on exercise 5 in codewarsChallenge5.js

--- codewarsChallenge5.js
+++ codewarsChallenge5.js
@@ -1,3 +1,8 @@
+// Title: Count Occurrences of a Value in an Array  
+
+// Summary: This code defines a function to count how many times a specific value appears in an array.  
+// In this case, it counts the occurrences of the number 100 in the scores array.  
+
 let scores = [100, 90, 85, 100, 60, 85, 100, 90, 55, 75, 60];  
 
 
