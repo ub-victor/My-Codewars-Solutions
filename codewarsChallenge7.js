54d7dad877f5c29404f575cfec48d58e9666fcd8
ub-victor <victoireushindi371@gmail.com>
2025-01-21 14:26:27 +0200

    Create file to codewarsChallenge7.js and add an exercise in it

---
+++ codewarsChallenge7.js
@@ -0,0 +1,11 @@
+let sampleObject = {
+    id: 10, 
+    delay: 20,
+    name: "en to tre",
+    isPresent: true,
+    delay: 50
+};
+sampleObject.delay = sampleObject.delay * 2;
+console.log(sampleObject.id);   
+console.log(sampleObject.name);   
+console.log(sampleObject.delay);   
