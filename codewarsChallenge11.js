2de7a7b739cdc414e98fbe62e74a937309296196
ub-victor <victoireushindi371@gmail.com>
2025-01-22 11:38:16 +0200

    Add a other method in  codewarsChallenge11.js so that it looks great

--- codewarsChallenge11.js
+++ codewarsChallenge11.js
@@ -1,5 +1,5 @@
 let contact = {
-    email_1: "RonaldSMurphy@freepost.org",
+    email_1: "victoireushindi371@gmail.com",
     email_2: "rsmurphy@briazz.com"
 };
 for(i=1; i<=2; i++) {
@@ -7,3 +7,12 @@ for(i=1; i<=2; i++) {
     console.log(key);
     console.log(contact[key]);
 }
+//actualy we can improve this code by using this structure
+/*for(i=1; i<=2; i++) {
+    let key = "email_" + i;
+    console.log(`${key}: ${contact[key]}`);
+}
+output:
+    email_1: victoire
+    email_2: Bihame
+*/
