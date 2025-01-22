81683588a7588bce439891b1949f2c483680582d
ub-victor <victoireushindi371@gmail.com>
2025-01-22 11:03:57 +0200

    Create an exercise in  codewarsChallenge11.js

---
+++ codewarsChallenge11.js
@@ -0,0 +1,9 @@
+let contact = {
+    email_1: "RonaldSMurphy@freepost.org",
+    email_2: "rsmurphy@briazz.com"
+};
+for(i=1; i<=2; i++) {
+    let key = "email_" + i;
+    console.log(key);
+    console.log(contact[key]);
+}
