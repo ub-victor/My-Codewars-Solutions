665394db202c33ee8d4d79836df9a95991c2ed1e
ub-victor <victoireushindi371@gmail.com>
2025-01-22 10:51:30 +0200

    Create a div and h3 and p to codewarsChallenge9.js in html

---
+++ codewarsChallenge10.js
@@ -0,0 +1,10 @@
+let contact = {
+    "first name": "Ronald"
+};
+contact["first name"] = "Tim";
+//contact.first name = "Tim";    // SyntaxError: Unexpected identifier
+//contact."first name" = "Tim";    // SyntaxError: Unexpected string
+
+console.log(contact["first name"]);    // Tim
+// Verify the update and log the entire object in a readable format  
+console.log(JSON.stringify(contact, null, 2)); // Converts the contact object to a JSON string with 2-space indentation for better readability  
