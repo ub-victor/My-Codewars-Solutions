3b87281eb427ad3cbeeaae8fffed5ff4774aed7c
ub-victor <victoireushindi371@gmail.com>
2025-01-23 10:59:55 +0200

    Add content in file script on CodewarsChallenge12.js

---
+++ codewarsChallenge13.js
@@ -0,0 +1,11 @@
+let contact = {  
+    email_1: "bim",   
+    name: "Ushindi",  
+    school: "AUCA"  
+}
+
+
+if(!contact.notes) { 
+    contact.notes = "something really important";
+}
+console.log(contact.notes);
