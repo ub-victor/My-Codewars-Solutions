f250a93c3a280701335e190752ec3c2db82e993d
ub-victor <victoireushindi371@gmail.com>
2025-01-23 12:47:49 +0200

    script 14 added content

---
+++ codewarsChallenge14.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const pi = 3.142;
+
+    function calcArea(radius) {
+        console.log("The area of the circle is: " + pi * radius * radius);
+    }
+    calcArea(5);
+}
