588839d5ffdfb21f7c3e2971e43bda8d9b926f3c
ub-victor <victoireushindi371@gmail.com>
2025-01-24 11:28:25 +0200

    Debage this code

--- codewarsChallenge3.js
+++ codewarsChallenge3.js
@@ -1,11 +1,9 @@
-let numbers = [2, -4, 6, 8, -3, 11, -78];
+let nums = [2, -4, 6, 8, -3, 11, -78];  
 
-// Step 1: Change negatives to positives and vice versa
+// Step 1: Change negatives to positives and vice versa  
+let changedSigns = nums.map(num => num * -1);   
 
-let changedSigns = numbers.map(num => num * -1); 
-
-// Step 2: Sort the new array in ascending order
-
-let sortedArray = changedSigns.sort((a, b) => a - b);
+// Step 2: Sort the new array in ascending order  
+let sortedArray = changedSigns.sort((a, b) => a - b);  
 
 console.log(sortedArray);
