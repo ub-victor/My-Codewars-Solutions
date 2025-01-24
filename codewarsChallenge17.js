8c039adcfd29240baa52666b9af46d86474a9bd9
ub-victor <victoireushindi371@gmail.com>
2025-01-24 11:53:32 +0200

    add content in the Script

--- codewarsChallenge17.js
+++ codewarsChallenge17.js
@@ -1,5 +1,6 @@
-window.onload = function() {
-    var nums1 = [1, 2, 3, 4, 5];
-    var nums2 = [...num1,6, 7, 8, 9, 10];
-    console.log(nums2);
-}
+window.onload = function () {  
+    var nums1 = [1, 2, 3, 4, 5]; // Original array  
+    var nums2 = [...nums1, 6, 7, 8, 9, 10]; // New array created by spreading nums1 and adding new elements  
+  
+    console.log(nums2); // Outputs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+  }
