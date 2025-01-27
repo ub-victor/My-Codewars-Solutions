0ebff7b9658427881880d3240086b797a2ca32bc
ub-victor <victoireushindi371@gmail.com>
2025-01-27 11:59:57 +0200

    add code into script 35

---
+++ codewarsChallenge35.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const multiplyAndLog = (a, b)=>{
+        const result = a * b;
+        console.log(result);
+        return result;
+    };
+    multiplyAndLog(3, 5);
+}
