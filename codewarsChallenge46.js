62c2a5b634b53f304c4de88233199047093422c1
ub-victor <victoireushindi371@gmail.com>
2025-02-10 11:31:43 +0200

    add code  into script 46

---
+++ codewarsChallenge46.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    const student = {
+        name: 'Richard kalehoff',
+        guardian: 'Mr. kalehoff'
+    };
+    const teacher = {
+        name: 'Mrs. Wilson',
+        room: 'N231'
+    };
+
+    let message = student.name + " please see " + teacher.name+ " in " + teacher.room +
+    " to pick up your card .";
+    console.log(message);
+}
