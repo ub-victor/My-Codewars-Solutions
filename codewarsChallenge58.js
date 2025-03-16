ef68227de36264d7bb1a4a2528fe9a1e2d23fd7e
ub-victor <victoireushindi371@gmail.com>
2025-03-16 16:13:17 +0200

    add code in Script 59

---
+++ codewarsChallenge58.js
@@ -0,0 +1,14 @@
+// The function takes an object as a parameter and destructures it
+    // to extract the 'scoops' and 'toppings' properties.
+    // If 'scoops' is not provided, it defaults to 1.
+    // If 'toppings' is not provided, it defaults to an array containing 'Hot Fudge'.
+
+function createSundae({scoops = 1, toppings = ['Hot Fudge']}){
+    const scoopText = scoops === 1 ? 'scoop' : "scoops";
+    return `Your sundae ${scoops} ${scoopText} with ${toppings.join(' and ')} toppings.`
+}
+
+console.log({}); // Your sundae had 1 scoop with Hot Fudge toppings.
+console.log({scoops: 2}); // Your sundae had 2 scoops with Hot Fudge toppings.
+console.log({scoops : 2, toppings: ['Sprinkles']}) // Your sundae has 2 scoops with Sprinkles toppings.
+console.log({toppings: ['Cookie Dough']}) // Your sundae has 1 scoop with Cookie Dough toppings.
