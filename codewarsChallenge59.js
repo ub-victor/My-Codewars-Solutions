e99b483abbe19a7fd15868ac7a89caf31a9e66bc
ub-victor <victoireushindi371@gmail.com>
2025-03-17 00:13:43 +0200

    add code in Script 59

--- codewarsChallenge59.js
+++ codewarsChallenge59.js
@@ -2,3 +2,18 @@
 //         access the properties and methose of a parent(supperClass)
 //         this = this object
 //         super = the parent
+
+class Animal{
+
+}
+class Rabbit extends Animal{
+
+}
+
+class Fish extends Animal{
+
+}
+
+class Hawk extends Animal{
+
+}
