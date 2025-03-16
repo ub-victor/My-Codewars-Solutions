4d47247abaa0a0a1d77d4d255f722170418f7f05
ub-victor <victoireushindi371@gmail.com>
2025-03-17 00:11:25 +0200

    add code in Script 59

---
+++ codewarsChallenge59.js
@@ -0,0 +1,4 @@
+// Super = keyword is used in classes to call the constructor or
+//         access the properties and methose of a parent(supperClass)
+//         this = this object
+//         super = the parent
