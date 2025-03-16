eb61dfb84507c68de3254058f2cf004002e2970f
ub-victor <victoireushindi371@gmail.com>
2025-03-17 00:23:04 +0200

    add code in Script 59

--- codewarsChallenge59.js
+++ codewarsChallenge59.js
@@ -4,16 +4,35 @@
 //         super = the parent
 
 class Animal{
+    constructor(){
+
+    }
 
 }
 class Rabbit extends Animal{
+    constructor(name, age, runSpeed){
+        this.name = name;
+        this.age = age;
+        this.runSpeed = runSpeed;
 
+    }
 }
 
 class Fish extends Animal{
+    constructor(name, age, swimSpeed){
+        this.name = name;
+        this.age = age;
+        this.swimSpeed = swimSpeed;
 
+    }
 }
 
 class Hawk extends Animal{
+    constructor(name, age, flySpeed){
+        this.name = name;
+        this.age = age;
+        this.flySpeed = runSpeed;
+
+    }
 
 }
