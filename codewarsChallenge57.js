727f5088164afcbbfd3332a965be2f8ba927e675
ub-victor <victoireushindi371@gmail.com>
2025-03-16 15:17:05 +0200

    Intial Commit

--- codewarsChallenge57.js
+++ codewarsChallenge57.js
@@ -2,7 +2,11 @@ function createGrid([width = 5, height = 5]){
     return `Generates a ${width} x ${height} grid`
 }
 
-createGrid([]); // Generates a 5 x 5 grid
-createGrid([2]); // Generates a 2 x 5 grid
-createGrid([2, 3]); // Generates a 2 x 3 grid
-createGrid([undefined, 3]); // Generates a 5 x 3 grid
+console.log(createGrid([])); // Generates a 5 x 5 grid
+console.log(createGrid([2])); // Generates a 2 x 5 grid
+console.log(createGrid([2, 3])); // Generates a 2 x 3 grid
+console.log(createGrid([undefined, 3])); // Generates a 5 x 3 grid
+
+// There is a problem with this though, the following code will not work:
+
+createGrid(); // throws an error
