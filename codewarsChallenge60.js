8459b87c5ebcd7f32a0372462fed6657a2923e2e
ub-victor <victoireushindi371@gmail.com>
2025-03-17 12:27:32 +0200

    Add a comment in html on script 59

--- codewarsChallenge60.js
+++ codewarsChallenge60.js
@@ -1,3 +1,14 @@
 class Tree {
-    constructor(size = "10", leaves = {spring: 'green':})
+    constructor(size = "10", leaves = {spring: 'green', summer: "green", fall: "orange", winter: null}){
+        this.size = size;
+        this.leaves = leaves;
+        this.leafColar = this.leafColar;
+    }
+    changeSeason(season){
+        this.leafColar = this.leaves[season] 
+        if(season === "spring"){
+            this.size += 1;
+        }
+    }
 }
+
