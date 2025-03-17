241192cff69935d1b3764edc4e2df1215fbbcc33
ub-victor <victoireushindi371@gmail.com>
2025-03-17 13:49:09 +0200

    Add a comment in html on script 59

--- codewarsChallenge60.js
+++ codewarsChallenge60.js
@@ -2,10 +2,12 @@ class Tree {
     constructor(size = "10", leaves = {spring: 'green', summer: "green", fall: "orange", winter: null}){
         this.size = size;
         this.leaves = leaves;
-        this.leafColar = null;
+        this.leafColor = null;
     }
+    // This method will change the leaf color based on the season provided.
+    /*This defines a function named changeSeason that takes a parameter called season. This parameter is expected to be one of the seasons (like 'spring', 'summer', 'fall', or 'winter'). */
     changeSeason(season){
-        this.leafColar = this.leaves[season] 
+        this.leafColor = this.leaves[season] 
         if(season === "spring"){
             this.size += 1;
         }
@@ -14,7 +16,7 @@ class Tree {
 
 class Maple extends Tree {
     constructor(syrupQty = 15, size, leaves){
-        super(size, leaves);
+        super(size, leaves);//Calls the parent Tree constructor using super(), passing the size and leaves parameters to initialize the inherited properties.
         this.syrupQty = syrupQty;
     }
     changeSeason(season){
@@ -30,6 +32,7 @@ class Maple extends Tree {
 
 const myMaple = new Maple(15, 5);
 
-console.log(changeSeason('fall'))
-console.log(changeSeason())
-console.log(changeSeason('spring'))
+console.log(changeSeason('fall'))// 15,5
+console.log(changeSeason())//15,5
+console.log(changeSeason('spring'))//16,6
+console.log(myMaple.gatherSyrup()); //13 ,6
