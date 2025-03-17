58c114e87faa7d0c3d70f2ab7a11217e422575a0
ub-victor <victoireushindi371@gmail.com>
2025-03-17 14:02:10 +0200

    Add a comment in html on script 59

--- codewarsChallenge60.js
+++ codewarsChallenge60.js
@@ -2,7 +2,7 @@ class Tree {
     constructor(size = "10", leaves = {spring: 'green', summer: "green", fall: "orange", winter: null}) {  
         this.size = size;  
         this.leaves = leaves;  
-        this.leafColor = null; // Note: Fixed the typo from leafColar to leafColor  
+        this.leafColor = null;   
     }  
 
     changeSeason(season) {  
