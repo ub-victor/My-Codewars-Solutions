41abe088830108e0b92cdee1fc73f93c5289c7cf
ub-victor <victoireushindi371@gmail.com>
2025-03-19 13:59:32 +0200

    Add  code Script 65

--- codewarsChallenge65.js
+++ codewarsChallenge65.js
@@ -1,2 +1,18 @@
 const digits = [1,2,3,4,5,6,7,8,9];
+// Create an iterator for the digits array
 const arrayIterator = digits[Symbol.iterator]();
+
+// The parentheses () after digits[Symbol.iterator] are used to call the iterator function. Here's a detailed explanation:
+
+// In JavaScript, arrays have a built-in method called Symbol.iterator which returns an iterator object. This iterator object allows you to traverse the array one element at a time.
+
+//Here's a breakdown of the code:
+
+// digits is an array.
+// digits[Symbol.iterator] accesses the iterator method of the array.
+// digits[Symbol.iterator]()Symbol.iterator calls this method, which returns an iterator object.
+// The iterator object has a next method that you can use to get the next value in the array. Each call to next returns an object with two properties: value (the next value in the array) and done (a boolean indicating whether the end of the array has been reached).
+
+// Here's an example of how you might use the iterator:
+
+// In summary, the parentheses () are used to call the Symbol.iterator method, which returns an iterator object for the digits array.
