ec012d21c0e73d9aa9a7e323628ba0682c463857
ub-victor <victoireushindi371@gmail.com>
2025-03-19 12:42:25 +0200

    Add  code Script 63

---
+++ codewarsChallenge63.js
@@ -0,0 +1,8 @@
+const bowl = {
+    /* By changing the bowl’s properties to use symbols, each property is a unique Symbol and the first banana doesn’t get overwritten by the second banana.*/
+    [Symbol('apple')]: { color: 'red', weight: 136.078 },
+    [Symbol('banana')]: { color: 'yellow', weight: 183.15 },
+    [Symbol('orange')]: { color: 'orange', weight: 170.097 },
+    [Symbol('banana')]: { color: 'yellow', weight: 176.845 }
+  };
+  console.log(bowl);
