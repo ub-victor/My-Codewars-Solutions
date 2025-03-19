ece9f1bc33233f55ae50b668ffc467cb92cf7184
ub-victor <victoireushindi371@gmail.com>
2025-03-19 12:44:41 +0200

    Add  code Script 63

--- codewarsChallenge63.js
+++ codewarsChallenge63.js
@@ -1,3 +1,16 @@
+/*
+  const bowl = {
+
+  'apple': { color: 'red', weight: 136.078 },
+  'banana': { color: 'yellow', weight: 183.151 },
+  'orange': { color: 'orange', weight: 170.097 },
+  'banana': { color: 'yellow', weight: 176.845 }
+};
+console.log(bowl);
+
+Instead of adding another banana to the bowl, our previous banana is overwritten by the new banana being added to the bowl. To fix this problem, we can use symbols.
+  */
+
 const bowl = {
     /* By changing the bowl’s properties to use symbols, each property is a unique Symbol and the first banana doesn’t get overwritten by the second banana.*/
     [Symbol('apple')]: { color: 'red', weight: 136.078 },
