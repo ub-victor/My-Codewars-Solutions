99977d08b178223ef2462387417ddd21c1dd4fd8
ub-victor <victoireushindi371@gmail.com>
2025-03-27 13:53:03 +0200

    Add code in script 66

---
+++ codewarsChallenge66.js
@@ -0,0 +1,4 @@
+const months = new Set(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+
+console.log(months.has('January')); // true
+console.log(months.size);
