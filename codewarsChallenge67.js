1664c4440875a713d3dd2c57dfb2b9bb69c609fe
ub-victor <victoireushindi371@gmail.com>
2025-03-27 14:18:59 +0200

    Add code in script 67

---
+++ codewarsChallenge67.js
@@ -0,0 +1,7 @@
+const months = new Set(['January', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+
+const iterator = months.values();
+iterator.next(); // { value: 'January', done: false }
+
+
+ 
