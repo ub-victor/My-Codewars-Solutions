521fa85d40adc107908a7f4581eb1a4a3b8b15e2
ub-victor <victoireushindi371@gmail.com>
2025-03-27 14:49:10 +0200

    Add code in script 69

---
+++ codewarsChallenge69.js
@@ -0,0 +1,14 @@
+// Create a Set object  
+let myFavoriteFlavors = new Set();  
+
+// Add flavors to the Set  
+myFavoriteFlavors.add("chocolate chip");  
+myFavoriteFlavors.add("cookies and cream");  
+myFavoriteFlavors.add("strawberry");  
+myFavoriteFlavors.add("vanilla");  
+
+// Remove "strawberry" from the Set  
+myFavoriteFlavors.delete("strawberry");  
+
+// Optionally, log the Set to see the remaining flavors  
+console.log(myFavoriteFlavors);  
