06f69e0daea711d8a91110f38301142959572068
ub-victor <victoireushindi371@gmail.com>
2025-04-06 16:59:58 +0200

    Add code in script 70

--- codewarsChallenge70.js
+++ codewarsChallenge70.js
@@ -2,7 +2,7 @@ let student1 = { name: 'James', age: 26, gender: 'male' };
 let student2 = { name: 'Julia', age: 27, gender: 'female' };
 let student3 = { name: 'Richard', age: 31, gender: 'male' };
 
-const roster = [student1, student2, student3]; // Create an array of student objects
+/* const roster = [student1, student2, student3]; // Create an array of student objects
 
 const getStudentNames = (roster) => {
     return roster.map(student => student.name); // Extract the 'name' property from each student object
@@ -13,3 +13,5 @@ const getStudentAges = (roster) => {
 console.log(getStudentNames(roster)); // Log an array of student names: ['James', 'Julia', 'Richard']
 console.log(getStudentAges(roster)); // Log an array of student ages: [26, 27, 31]
 console.log(roster); // Log the original roster array of student objects
+
+*/
