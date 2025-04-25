36ebc7e01a37ec7149b76434c87fbdb41fd003cb
ub-victor <victoireushindi371@gmail.com>
2025-04-26 00:46:20 +0200

    Initial Commit

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const currentYear = new Date().getFullYear();
-document.getElementsByClassName("current-year").textContent = currentYear;
+document.getElementById("current-year").textContent = currentYear;
