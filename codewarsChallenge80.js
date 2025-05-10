aa025de31fa2d7130bf9d9f1a9d1882a34769530
ub-victor <victoireushindi371@gmail.com>
2025-05-10 13:21:54 +0200

    Initial commit

---
+++ codewarsChallenge80.js
@@ -0,0 +1,12 @@
+const food = 'salad';
+
+switch (food) {
+  case 'oyster':
+    console.log('The taste of the sea 🦪');
+    break;
+  case 'pizza':
+    console.log('A delicious pie 🍕');
+    break;
+  default:
+    console.log('Enjoy your meal');
+}
