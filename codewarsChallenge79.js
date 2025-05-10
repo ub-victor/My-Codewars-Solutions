3fcd723bbd9f17737bb02db0a60c9cd2643121f1
ub-victor <victoireushindi371@gmail.com>
2025-05-10 12:01:32 +0200

    Initial commit

--- codewarsChallenge79.js
+++ codewarsChallenge79.js
@@ -3,3 +3,7 @@
 const weeksInYear = Math.floor(365/7);
 
 // Calculates the number of days left over after 365 is divided by 7
+
+const daysLeftOver = 365 % 7 ;
+
+console.log("A year has " + weeksInYear + " weeks and "+ daysLeftOver + " days");
