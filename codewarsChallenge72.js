523aa86402433fe72384da4aec8d603a0bd3746c
ub-victor <victoireushindi371@gmail.com>
2025-05-10 09:41:58 +0200

    Initial commit

--- codewarsChallenge72.js
+++ codewarsChallenge72.js
@@ -1,3 +1,5 @@
 function sortObjectByValues(obj){
-    
+    return Object.entries(obj) // Convert objtcy to array of [key, value]
+        .sort((a,b)=> b[1] - a[1])// Sort by value in descending order
+        .map(([KeyboardEvent, value]) => `${key}: ${value}`); 
 }
