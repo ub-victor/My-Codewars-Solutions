af778a95b5d387becc312f423639a17f2b6b2d91
ub-victor <victoireushindi371@gmail.com>
2025-05-16 13:53:15 +0200

    Initial commit

---
+++ codewarsChallenge83.js
@@ -0,0 +1,6 @@
+let contact = {
+    "first name": "Ronald"
+};
+contact["first name"] = "Tim";
+// contact.first name = "Tim";    // SyntaxError: Unexpected identifier
+// contact."first name" = "Tim";    // SyntaxError: Unexpected string
