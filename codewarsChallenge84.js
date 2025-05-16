4cb9d8c66b14c1330b55cddfc583c81e4a2cf7e9
ub-victor <victoireushindi371@gmail.com>
2025-05-16 19:32:22 +0200

    Initial commit

---
+++ codewarsChallenge84.js
@@ -0,0 +1,12 @@
+// Define a contact object without a notes property
+let contact = {
+  name: "John Doe",
+  email: "john@example.com"
+};
+
+// Check if contact.notes is falsy (undefined, null, "", etc.)
+if (!contact.notes) {
+  contact.notes = "something really important";
+}
+
+console.log(contact);
