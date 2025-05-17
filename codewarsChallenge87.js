bc666298b5350b3448e4a8bc1ead3c947d2f66fd
ub-victor <victoireushindi371@gmail.com>
2025-05-17 15:27:42 +0200

    Initial commit

---
+++ codewarsChallenge87.js
@@ -0,0 +1,16 @@
+function findAverage(array) {
+  // your code here
+ 
+  let sum = 0;
+      for(i= 0; i < array.length; i++){
+          sum+= array[i];
+  
+        } return sum/ array.length;
+  
+ 
+  
+  }
+
+ console.log(findAverage([5,5,5,5]));
+  
+ 
