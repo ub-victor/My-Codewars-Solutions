957bf6554837f1d881410bc9e1a65f017ba1ca61
ub-victor <victoireushindi371@gmail.com>
2025-05-17 02:16:15 +0200

    Initial commit

---
+++ codewarsChallenge86.js
@@ -0,0 +1,12 @@
+let contact = {
+    tel: "207-662-5412",
+    email: "RonaldSMurphy@freepost.org"
+};
+for(x in contact) {
+    // print property name cuz x refers to the property name
+    console.log(x);
+    // print property value
+    console.log(contact[x]);
+    // print property and value
+     console.log(x + ": "+ contact[x]);
+}
