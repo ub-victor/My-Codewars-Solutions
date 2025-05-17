6922498ab8b85a839cef6eca949e902e4bf124a5
ub-victor <victoireushindi371@gmail.com>
2025-05-17 17:19:39 +0200

    Initial commit

--- codewarsChallenge89.js
+++ codewarsChallenge89.js
@@ -3,5 +3,7 @@ const array1 = [1, 2, 3, 4, 5];
 // 0 + 1 + 2 + 3 + 4 + 5 = 15
 const  initialValue = 0;
 const sumWithInitial = array1.reduce((acc, curr) => acc + curr, initialValue,);
+//const argWithInitial = array1.reduce((acc, curr) => acc + curr, initialValue)/ array1.length;
+//console.log(argWithInitial); // 3
 
 console.log(sumWithInitial); // 15
