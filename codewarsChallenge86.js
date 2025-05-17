0f34bcacd96d28513e2628745c0ccf9e2742ed7d
ub-victor <victoireushindi371@gmail.com>
2025-05-17 02:18:11 +0200

    Initial commit

--- codewarsChallenge86.js
+++ codewarsChallenge86.js
@@ -5,8 +5,8 @@ let contact = {
 for(x in contact) {
     // print property name cuz x refers to the property name
     console.log(x);
-    // print property value
-    console.log(contact[x]);
-    // print property and value
-     console.log(x + ": "+ contact[x]);
+    // // print property value
+    // console.log(contact[x]);
+    // // print property and value
+    //  console.log(x + ": "+ contact[x]);
 }
