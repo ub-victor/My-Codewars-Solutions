15a19cab3a008a660187d5d65f323cabfef4b79f
ub-victor <victoireushindi371@gmail.com>
2025-05-20 20:05:46 +0200

    Initial commit

--- codewarsChallenge90.js
+++ codewarsChallenge90.js
@@ -8,4 +8,10 @@ console.log(sayHello(["Victor", "Ushindi"], "New york", "America"));
 // let use arrow function
 /*const sayHelle = (name, city, state) => {
     return `Hello, ${name.join(", ")}! Welcome to ${city} ${state}`;
-}*/
+}*/ //or
+
+/*
+function sayHello(name, city, state) {
+  const fullName = name.join(' ');
+  return `Hello, ${fullName}! Welcome to ${city}, ${state}!`;
+} */
