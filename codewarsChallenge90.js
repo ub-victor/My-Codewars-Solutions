ac350ff631316275083860619af678290b51aa5b
ub-victor <victoireushindi371@gmail.com>
2025-05-20 19:23:25 +0200

    Initial commit

---
+++ codewarsChallenge90.js
@@ -0,0 +1,9 @@
+function sayHello( name, city, state ) {
+  return `Hello, ${name.join(", ")}! Welcome to ${city} ${state}`;
+}
+
+console.log(sayHello(["Victor", "Ushindi"], "New york", "America"));
+
+
+
+// let use
