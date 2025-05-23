a9d5fe9ff7ef7fcc4633ef548d79146c19a07df1
ub-victor <victoireushindi371@gmail.com>
2025-05-23 05:14:14 +0200

    Initial commit

--- codewarsChallenge92.js
+++ codewarsChallenge92.js
@@ -8,4 +8,3 @@ console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", 
 
 
 const twoSort = s => s.sort()[0].split('').join('***');
-console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]));
