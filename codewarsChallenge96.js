422efcec47809a715a3e1440846e179d8807d872
ub-victor <victoireushindi371@gmail.com>
2025-05-24 16:36:47 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -46,3 +46,5 @@ console.log("banana boat tomato".match(pattern)); // ["b", "b"] - matches 'b' at
 
 // /b/ finds any 'b' in the string.
 // /\bb\w*/ finds whole words starting with 'b'.
+// \w*: Match zero or more word characters (letters, digits, underscore).
+// g: Find all matches in the string, not just the first.
