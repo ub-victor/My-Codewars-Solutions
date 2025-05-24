2fa325a6c28b40e3646b75be6c3a8b40605545ae
ub-victor <victoireushindi371@gmail.com>
2025-05-24 16:35:13 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -42,3 +42,7 @@ console.log(pattern.test("cats")); // false
  */
 let pattern = /\bb\w*/g; // The first 'b' is a word boundary (\b), ensuring 'b' is at the start of a word; the second 'b' matches the letter 'b' itself.
 console.log("banana boat tomato".match(pattern)); // ["b", "b"] - matches 'b' at the start of 'banana' and 'boat' and ignores 'tomato' since it doesn't start with 'b' by ouptputting an array of matches
+
+
+// /b/ finds any 'b' in the string.
+// /\bb\w*/ finds whole words starting with 'b'.
