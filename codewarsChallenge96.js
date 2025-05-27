2f388232a6e09c811905fa1b4c1e2ddb9fcc1f40
ub-victor <victoireushindi371@gmail.com>
2025-05-28 00:33:47 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -74,12 +74,16 @@ function leave(){
   console.log("leave")
 }
 
+// Title:
+// Callback Function Example: Summing Two Numbers
+
+// Summary:
+// This code defines a sum function that adds two numbers and passes the result to a callback function. It demonstrates using callbacks by printing the sum to the console with a separate displayConsole function.
 
 function sum(callback, x, y){
     let result  = x+y
     callback(result);
 }
-
 sum(displayConsole, 4, 4);
 
 function displayConsole(result){
