26dddeefc958d4e6908c460f41119c5d7d0a4ebc
ub-victor <victoireushindi371@gmail.com>
2025-05-28 00:19:28 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -74,3 +74,17 @@ function wait(){
 function leave(){
   console.log("leave")
 }
+
+
+function sum(callback, x, y){
+    let result  = x+y
+    callback(result);
+}
+
+sum(displayConsole, 4, 4);
+
+function displayConsole(result){
+  console.log("my result is")
+}
+
+ 
