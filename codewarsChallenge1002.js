7495784eb57bd0ef452a8730b6c79fbd13e5ab4e
ub-victor <victoireushindi371@gmail.com>
2025-05-28 22:24:36 +0200

    Initial commit

--- codewarsChallenge1002.js
+++ codewarsChallenge1002.js
@@ -1,10 +1,3 @@
-/**
- * Checks if elements in an array represent vowel character codes and replaces them with the corresponding vowel character.
- *
- * @param {number[]} a - An array of numbers representing character codes.
- * @returns {(number|string)[]} A new array where numbers corresponding to vowels are replaced by their character, others remain unchanged.
- */
-
 function isVow(a){
   return a.map(x =>{
   if(/[aeiou]/.test(String.fromCharCode(x))){
