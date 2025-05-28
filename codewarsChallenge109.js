7c4f0c4c738a03476dc8861377f7fe5521c8a06b
ub-victor <victoireushindi371@gmail.com>
2025-05-29 01:13:44 +0200

    Initial commit

--- codewarsChallenge109.js
+++ codewarsChallenge109.js
@@ -6,3 +6,15 @@ function extractVowels(name) {
 
 // Example:
 console.log(extractVowels("Victoire")); // Output: "ioie"
+
+
+//  How .filter() works:
+// .filter(callback) loops over every element in the array. For each element:
+
+// It runs the callback(element)
+
+// If the callback returns true, the element is included in the new array
+
+// If the callback returns false, the element is skipped
+
+
