50fa6b90988da9b9e3fc199fffe80e41ec082115
ub-victor <victoireushindi371@gmail.com>
2025-05-28 12:19:57 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -3,14 +3,6 @@ let pattern  = /hello/
 console.log(pattern.test("hello world")); // true
 console.log(pattern.test("hi there")); // false  
 
-function sum(callback, x, y){
-    let result  = x+y
-    callback(result);
-}
-sum(displayConsole, 4, 4);
 
-function displayConsole(result){
-  console.log("my result is" + result);
-}
 
  
