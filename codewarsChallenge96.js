b8e676500375beaa51f639d708bec2f3f349329f
ub-victor <victoireushindi371@gmail.com>
2025-05-28 12:17:23 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -4,7 +4,30 @@ console.log(pattern.test("hello world")); // true
 console.log(pattern.test("hi there")); // false  
 
 
-let pattern = /\bb\w*/g; // The first '
+
+
+ 
+/**
+ * 
+ *
+
+ */
+let pattern = /\bb\w*/g; // The first 'b' is a word boundary (\b), ensuring 'b' is at the start of a word; the second 'b' matches the letter 'b' itself.
+console.log("banana boat tomato".match(pattern)); // ["b", "b"] - matches 'b' at the start of 'banana' and 'boat' and ignores 'tomato' since it doesn't start with 'b' by ouptputting an array of matches
+
+
+/**
+ * 
+ *
+ * 
+ */
+
+
+// Title:
+// Callback Function Example: Summing Two Numbers
+
+// Summary:
+// This code defines a sum function that adds two numbers and passes the result to a callback function. It demonstrates using callbacks by printing the sum to the console with a separate displayConsole function.
 
 function sum(callback, x, y){
     let result  = x+y
