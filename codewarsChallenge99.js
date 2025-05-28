2da5d714d1a73c6a890446bf69a96a68a3db1d72
ub-victor <victoireushindi371@gmail.com>
2025-05-28 12:02:25 +0200

    Initial commit

---
+++ codewarsChallenge99.js
@@ -0,0 +1,2 @@
+let pattern = /\bb\w*/g; // The first 'b' is a word boundary (\b), ensuring 'b' is at the start of a word; the second 'b' matches the letter 'b' itself.
+console.log("banana boat tomato".match(pattern)); // ["b", "b"] - matches 'b' at the start of 'banana' and 'boat' and ignores 'tomato' since it doesn't start with 'b' by ouptputting an array of matches
