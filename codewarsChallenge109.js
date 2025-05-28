1cbea7c0c41f7fe1a8a957cb7249e8971577b0f0
ub-victor <victoireushindi371@gmail.com>
2025-05-29 01:08:44 +0200

    Initial commit

--- codewarsChallenge109.js
+++ codewarsChallenge109.js
@@ -1,6 +1,6 @@
 function extractVowels(name) {
   return Array.from(name)
-    .filter(char => /[aeiou]/i.test(char))
+    .filter(char => /[aeiou]/i.test(char)) // The i flag makes it case-insensitive, so it matches both lowercase and uppercase vowels.
     .join('');
 }
 
