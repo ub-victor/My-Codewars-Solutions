b002f6aa837bd395dd81b49151d95e221091022b
ub-victor <victoireushindi371@gmail.com>
2025-05-28 22:50:23 +0200

    Initial commit

--- codewarsChallenge103.js
+++ codewarsChallenge103.js
@@ -1,12 +1,5 @@
-**Title:**  
-Convert Character Codes to Letters Using `map` in JavaScript
-
-**Summary:**  
-This code converts an array of character codes to their corresponding letters using `String.fromCharCode` and logs each letter to the console.
-
-
-let codes = [97, 98, 99, 100]; // 'a', 'b', 'c', 'd'
+const codes = [118, 105, 99, 116, 111, 105, 114, 101];
 codes.map(x => {
   console.log(String.fromCharCode(x));
 });
-// Outputs: 'a', 'b', 'c', 'd'
+// Output : // "victoire"
