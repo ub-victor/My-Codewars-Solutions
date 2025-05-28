baf0806ce45701901dd872755c49011416508193
ub-victor <victoireushindi371@gmail.com>
2025-05-29 00:09:41 +0200

    Initial commit

---
+++ codewarsChallenge107.js
@@ -0,0 +1,8 @@
+const sentence = "The quick brown fox jumps over the lazy do.";
+const index = 4;
+
+console.log(
+  `Character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt(index,)}`
+  );
+
+  
