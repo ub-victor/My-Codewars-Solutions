6356bd06e1899b967fe71bee6059db48701ac641
ub-victor <victoireushindi371@gmail.com>
2025-05-28 11:13:01 +0200

    Initial commit

--- codewarsChallenge96.js
+++ codewarsChallenge96.js
@@ -1,11 +1,3 @@
-/**
- * Title: Simple Regular Expression Pattern Matching Example
- * 
- * Summary: 
- * Demonstrates the use of a regular expression in JavaScript to test whether the string "hello"
- * appears at the beginning of a given input string. The code logs the result of testing two different
- * strings against the pattern.
- */
 let pattern  = /hello/
 
 console.log(pattern.test("hello world")); // true
