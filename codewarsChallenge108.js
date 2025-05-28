8c24d4f8ad7e6ea8c4157efb7b6ba13bb377c090
ub-victor <victoireushindi371@gmail.com>
2025-05-29 00:55:31 +0200

    Initial commit

--- codewarsChallenge108.js
+++ codewarsChallenge108.js
@@ -1,4 +1,6 @@
 function getCharCodes(name){
+    // Create an array of Unicode character codes for each letter in the string
+// Each 'char' is a single-character string, so we use charCodeAt(0)
     return Array.from(name).map(char => char.charCodeAt(0))
 } 
 
