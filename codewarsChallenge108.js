83f108094defc67998bea6d79459b75c682554a8
ub-victor <victoireushindi371@gmail.com>
2025-05-29 00:48:38 +0200

    Initial commit

--- codewarsChallenge108.js
+++ codewarsChallenge108.js
@@ -3,6 +3,7 @@ function getCharCodes(name){
 } 
 
 
+// Example usage:
 const name = "Victoire";
 const codes = getCharCodes(name);
 console.log(codes); // Output: [86, 105, 99, 116, 111, 105, 114, 101]
