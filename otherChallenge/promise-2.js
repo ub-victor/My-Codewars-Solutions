03081466db94a7e9fbfabd7dcdf9ec4114578f0c
ub-victor <victoireushindi371@gmail.com>
2025-06-03 23:47:17 +0200

    Initial commit

--- otherChallenge/promise-2.js
+++ otherChallenge/promise-2.js
@@ -14,3 +14,25 @@ function stepTwo(previousStep){
         },1000);
     })
 }
+
+
+function stepThree(previousStep){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve (`${previousStep} -> Step 3: Drained and added sauce`);
+        }, 1000)
+    })
+}
+
+stepOne()
+    .then((result1)=>{
+        console.log(result1);
+        return stepTwo(result1);
+    })
+    .then((result2)=>{
+        console.log(result2);
+        return stepThree(result2);
+    })
+    .then ((finalResult)=>{
+        console.log(finalResult);
+    })
