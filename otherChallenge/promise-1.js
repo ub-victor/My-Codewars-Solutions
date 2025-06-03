564d9ebbbaf5b0f5b4150f08ab1d8499775ebd93
ub-victor <victoireushindi371@gmail.com>
2025-06-03 16:16:10 +0200

    Initial commit

--- otherChallenge/promise-1.js
+++ otherChallenge/promise-1.js
@@ -1,19 +1,31 @@
-const done = true ; 
+const done = false ; 
 
 
 const isItDoneYet = new Promise((resolve, reject)=>{
     if(done){
         resolve("Here is the thing I built for you!");
     }else{
-        reject("Still eorking on something else!");
+        reject("Still working on something else!");
     }
 })
 
 
-isItDoneYet
-    .then(message =>{
-        console
-    })
-    .catch (error =>{
+// isItDoneYet
+//     .then(message =>{
+//         console.log(message);
+//     })
+//     .catch (error => {
+//         console.error(error);
+//     })
+
+// async /wait syntax
+
+
+async function checkIfDone() {
+    try {
+        const result = await isItDoneYet;
+        console.log(result);
+    } catch (error) {
         console.error(error);
-    })
+    }
+}
