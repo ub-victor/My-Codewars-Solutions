db428cd1e300f2e6b37ce2634c83923e8647c4d1
ub-victor <victoireushindi371@gmail.com>
2025-06-04 16:47:36 +0200

    Initial commit

--- otherChallenge/promise-5.js
+++ otherChallenge/promise-5.js
@@ -10,6 +10,10 @@ new promise((resolve, reject) => {
     throw new error("Something failed in the second then");
 })
 .then(result =>{
-    console.log("Third then:", result);
+    console.log("Third then:", result);// This will not run due to the error in the previous then
     return result + 1;
 })
+
+.catch(error => {
+    console.error("Caught an error:", error.message); // This will catch the error from the second then
+});
