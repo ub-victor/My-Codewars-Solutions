1026fbb77c2d9116f2e8f02fb20fa9c830b937a3
ub-victor <victoireushindi371@gmail.com>
2025-06-04 18:24:43 +0200

    Initial commit

---
+++ otherChallenge/promise-7.js
@@ -0,0 +1,19 @@
+new Promise ((resolve, reject) => {
+    resolve("Start");
+})
+.then(result=>{
+    console.log("1st then:", result);
+    return "Step 1"
+})
+.then(result=>{
+    console.log("2nd then:", result);
+    throw new error ("Something went wrong here!"); 
+})
+.then(result =>{
+    console.log("3rd then:", result); // This will not run due to the error in the previous then
+    return "Step 3";
+})
+
+.catch(error => {
+    console.error("Caught an error:", error.message); // This will catch the error from the second then
+}); 
