32983b428ebbaadd7ee20fea22b1959665996589
ub-victor <victoireushindi371@gmail.com>
2025-06-05 00:55:15 +0200

    Initial commit

---
+++ otherChallenge/promise-8.js
@@ -0,0 +1,7 @@
+// Create 3 simple Promises
+
+let p1 = new Promise((resolve, reject) => {
+    setTimeout()
+});
+let p2 = new Promise((resolve, reject) => {});
+let p3 = new Promise((resolve, reject) => {});
