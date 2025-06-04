a4d589de0c7eadf772a770e4d439fc3a07524897
ub-victor <victoireushindi371@gmail.com>
2025-06-05 01:38:59 +0200

    Initial commit

--- otherChallenge/promise-8.js
+++ otherChallenge/promise-8.js
@@ -1,7 +1,35 @@
-// Create 3 simple Promises
-
+// Create 3 simple promises
 let p1 = new Promise((resolve, reject) => {
-    setTimeout()
+  setTimeout(() => resolve("First finished"), 1000); // resolves after 1 second
 });
-let p2 = new Promise((resolve, reject) => {});
-let p3 = new Promise((resolve, reject) => {});
+
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Second finished"), 2000); // resolves after 2 seconds
+});
+
+let p3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Third finished"), 1500); // resolves after 1.5 seconds
+});
+
+// Orchestrate them using Promise.all
+Promise.all([p1, p2, p3])
+  .then(results => {
+    console.log("All done:", results); // Logs array of results from all promises
+  })
+  .catch(error => {
+    console.error("One failed:", error); // Will run if any promise fails
+  });
+
+  // Orchestrate them using promise.all
+
+// Orchestrate them using async/await
+// async function processPromises() {
+//     try{
+//         const results = await Promise.all([p1, p2, p3]);
+//         console.log("All done :", results); // Logs array of results from all promises
+//     }
+//     catch(error) {
+//         console.error("One failed:", error); // Will run if any promise fails
+//     }
+// }
+  
