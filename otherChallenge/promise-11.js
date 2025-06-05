7a263f5f0c1e844de0d1b6633c71f82c31a8f02d
ub-victor <victoireushindi371@gmail.com>
2025-06-06 01:32:42 +0200

    Initial commit

--- otherChallenge/promise-11.js
+++ otherChallenge/promise-11.js
@@ -10,3 +10,16 @@ function processData(data){
         setTimeout(() => resolve("Data processed"), 1000); // Simulate processing time
     });
 }
+
+fetchData()
+    .then(result =>{
+        return processData(result);
+    })
+    .then(response =>{
+        console.log("Final result", response);
+    })
+
+    .catch(error => {
+        console.error("Caught Error:", error);
+    });
+
