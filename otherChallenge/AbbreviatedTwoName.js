210a99ea8fbe9bdfc4d4a3878f04ca0072804d6b
ub-victor <victoireushindi371@gmail.com>
2025-06-05 22:53:09 +0200

    Initial commit

--- otherChallenge/AbbreviatedTwoName.js
+++ otherChallenge/AbbreviatedTwoName.js
@@ -9,4 +9,3 @@ console.log(abbrevName("Ushindi Bihame Victoire")); // "U.B.V"
 // Split results into an array of names
 // Map help in looping through each element of the array
 // Join return the result as a string with the first letter of each name separated by a dot
-
