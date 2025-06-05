bb2d0480ec483f7d080ab6d1b7f6bfea0de2dca1
ub-victor <victoireushindi371@gmail.com>
2025-06-05 03:46:21 +0200

    Initial commit

--- otherChallenge/AbbreviatedTwoName.js
+++ otherChallenge/AbbreviatedTwoName.js
@@ -1,3 +1,11 @@
 const abbrevName = name =>{
-    name.toUpperCase().split(' ')
+    name.toUpperCase().split(' ').map(element => element[0]).join('.')
 }
+
+
+abbrevName("Ushindi Bihame Victoire"); // "U.B.V"
+
+
+// Split results into an array of names
+// Map help in looping through each element of the array
+// Join return the result as a string with the first letter of each name separated by a dot
