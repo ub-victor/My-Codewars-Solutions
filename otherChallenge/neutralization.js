2ec367571322ae3d2c8e969b73466e009730fc8d
ub-victor <victoireushindi371@gmail.com>
2025-06-06 17:01:36 +0200

    Initial commit

---
+++ otherChallenge/neutralization.js
@@ -0,0 +1,38 @@
+console.log(neutralise('abc', 'abc')); // 'abc'
+console.log(neutralise('abc', 'abd')); // '0'
+
+function neutralise(s1, s2) {
+    let result = '';
+    for (let i = 0; i < s1.length; i++) {
+        const c1 = s1[i];
+        const c2 = s2[i];
+        if (c1 === c2) {
+            result += c1;
+        }else {
+            result += '0'
+        }
+    }
+    return result;
+}
+
+console.log(neutralise('++++', '++--'));
+console.log(neutralise('++--', '----')); 
+
+
+// an other way of doing it for the same problem but with different rules +// if both are + then return + // if both are - then return -// else return 0    
+
+function neutralise(s1, s2) {
+    let result = '';
+    for (let i = 0; i < s1.length; i++) {
+        const c1 = s1[i];
+        const c2 = s2[i];
+        if (c1 === '+' && c2 === '+') {
+            result += '+';
+        } else if (c1 === '-' && c2 === '-') {
+            result += '-';
+        } else {
+            result += '0';
+        }
+    }
+    return result;
+}
