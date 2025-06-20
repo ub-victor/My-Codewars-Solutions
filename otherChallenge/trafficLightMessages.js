31a63fab651fd90610e999832f71e77c6c88173e
ub-victor <victoireushindi371@gmail.com>
2025-06-20 23:27:49 +0200

    Initial commit

---
+++ otherChallenge/trafficLightMessages.js
@@ -0,0 +1,15 @@
+let color = 'yellow';
+
+switch (color) {
+  case 'red':
+    console.log('Stop!');
+    break;
+  case 'yellow':
+    console.log('Get Ready!');
+    break;
+  case 'green':
+    console.log('Go!');
+    break;
+  default:
+    console.log('Unknown color');
+}
