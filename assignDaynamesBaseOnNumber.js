6813c03dd0daa36236a9a071a6eb61f7f11bc9cb
ub-victor <victoireushindi371@gmail.com>
2025-06-20 23:12:26 +0200

    Initial commit

---
+++ assignDaynamesBaseOnNumber.js
@@ -0,0 +1,36 @@
+let dayNumber = 7 // Example input
+let dayName;
+
+switch (dayNumber){
+    case 1 :
+        dayName = 'Monday';
+        console.log(dayName);
+        break;
+    case 2: 
+        dayName = 'Tusday;';
+        console.log(dayName);
+        break;
+    case 3:
+        dayName = 'Wenesday';
+        console.log(dayName);
+        break;
+    case 4: 
+        dayName = 'Thursday';
+        console.log(dayName);
+        break;
+    case 5:
+        dayName ='Friday';
+        console.log(dayName);
+        break;
+    case 6: 
+        dayName = 'Satarday';
+        console.log(dayName);
+        break;
+    case 7: 
+        dayName = 'Sunday';
+        console.log(dayName);
+        break;
+    default:
+        dayName = 'Invalid day number';
+        console.log(dayName)
+}
