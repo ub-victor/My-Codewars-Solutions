1abaadafd66272a5e70fcc05a715eb81a172654b
ub-victor <victoireushindi371@gmail.com>
2025-06-20 23:25:28 +0200

    Initial commit

---
+++ otherChallenge/handleinvaliddaynumber:.js
@@ -0,0 +1,30 @@
+let dayNumber = 10; // invalid input
+let dayName;
+
+switch (dayNumber) {
+  case 1:
+    dayName = 'Monday';
+    break;
+  case 2:
+    dayName = 'Tuesday';
+    break;
+  case 3:
+    dayName = 'Wednesday';
+    break;
+  case 4:
+    dayName = 'Thursday';
+    break;
+  case 5:
+    dayName = 'Friday';
+    break;
+  case 6:
+    dayName = 'Saturday';
+    break;
+  case 7:
+    dayName = 'Sunday';
+    break;
+  default:
+    dayName = 'Invalid day number';
+}
+
+console.log(dayName); // Output: Invalid day number
