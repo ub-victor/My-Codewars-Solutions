9888898ea9d8fc475e815ad38cda0ac0a3fd92fa
ub-victor <victoireushindi371@gmail.com>
2025-06-20 23:57:22 +0200

    Initial commit

---
+++ otherChallenge/do...While.js
@@ -0,0 +1,14 @@
+let count = 0;  
+
+do {
+    console.log('count:', count);
+    count++;
+}while(count <= 3);
+
+
+// With the do while loop it sinple and easy
+// just start by declaring a varible and put in it the number you want it to start with kind of initializing the loop
+
+// then declare what the loop with do / output and the way to get increase
+
+// the to the while lope then do defined the condition to follow
