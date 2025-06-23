5f1d9a020268afe1687fa9567ba168e4ae8c3231
ub-victor <victoireushindi371@gmail.com>
2025-06-23 17:39:26 +0200

    Initial commit

---
+++ otherChallenge/objectAss.js
@@ -0,0 +1,16 @@
+let point0 = {x:10, y: 20 };
+let point2 = { ...point0};
+let point3 = { ...point0, z: 100};
+
+
+console.log(point0); // { x: 10, y: 20 }
+console.log(point2); // { x: 10, y: 20 }
+console.log(point3); // { x: 10, y: 20, z: 100 }
+console.log(point0 === point2); // false, point0 and point2 are different objects
+console.log(point0 === point3); // false, point0 and point3 are different objects
+console.log(point2 === point3); // false, point2 and point3 are different objects
+console.log(point2.x); // 10
+console.log(point2.y); // 20
+console.log(point3.z); // 100
+console.log(point3.x); // 10
+console.log(point3.y); // 20    
