efeee76855fa523121f0f61206489f34b5aeb75e
ub-victor <victoireushindi371@gmail.com>
2025-06-23 17:29:50 +0200

    Initial commit

--- otherChallenge/ObjectAssign.js
+++ otherChallenge/ObjectAssign.js
@@ -11,3 +11,4 @@ console.log(point1 === point2); // false, point1 and point2 are different object
 console.log(point2.x) // 10
 console.log(point2.y) // 20
 console.log(point2 === point0); // false, point2 is a new object with the same properties as point0
+console.log(point2);// { x: 10, y: 20 }
